831b82e0ea3cddf3dc037992bdedeb6e34fd3021
Fkn6mfU7egPd <machaco_0320@outlook.com>
2025-11-08 18:53:57 +0900

    accuAddを再実装

--- accurateMath.js
+++ accurateMath.js
@@ -1,48 +1,98 @@
 function accuAdd(a, b) {
-  var as = a.split("."),
-    bs = b.split(".");
-  var ai = as[0],
-    bi = bs[0];
-  var af = as[1] || "",
-    bf = bs[1] || "";
-  while (af.length < bf.length) af += "0";
-  while (bf.length < af.length) bf += "0";
+  if(a.includes("-")) {
+    if(b.includes("-")) {
+      if(a == "0" && b == "0") return "0";
+      return "-" + accuAdd(a.slice(1), b.slice(1));
+    }
+    else return accuSub(b, a.slice(1));
+  }
+  if(b.includes("-")) return accuSub(a, b.slice(1));
 
-  var carry = 0,
-    resf = "";
-  for (var i = af.length - 1; i >= 0; i--) {
-    var sum = Number(af[i]) + Number(bf[i]) + carry;
-    carry = sum >= 10 ? 1 : 0;
-    resf = (sum % 10) + resf;
+  let temp1 = 0, temp2 = 0;
+  if(a.includes(".") || b.includes(".")) {
+    for(let temp27 = 0; Math.max(a.length, b.length) > temp27; temp27++) {
+      if(a[a.length - 1 - temp27] === ".") temp1 = temp27;
+      if(b[b.length - 1 - temp27] === ".") temp2 = temp27;
+    }
+  }
+  let temp17 = a.replace(".", "") + "0".repeat(temp2 > temp1 ? temp2 - temp1 : 0);
+  let temp21 = b.replace(".", "") + "0".repeat(temp1 > temp2 ? temp1 - temp2 : 0);
+
+  let temp3 = "";
+  let temp30 = 0;
+  for(let temp27 = 0; Math.max(temp17.length, temp21.length) > temp27; temp27++) {
+    temp3 = ((Number(temp17[temp17.length - 1 - temp27] || "0") + Number(temp21[temp21.length - 1 - temp27] || "0") + temp30) % 10).toString() + temp3;
+    temp30 = Math.floor((Number(temp17[temp17.length - 1 - temp27] || "0") + Number(temp21[temp21.length - 1 - temp27] || "0") + temp30) / 10);
+  }
+  if(temp30 == 1) temp3 = "1" + temp3;
+
+  let accuCalcResult = "";
+  let temp27 = 0;
+  for(; Math.max(temp1, temp2) > temp27; temp27++) {
+    if(!(accuCalcResult === "" && temp3[temp3.length - 1 - temp27] === "0")) accuCalcResult = temp3[temp3.length - 1 - temp27] + accuCalcResult;
   }
 
-  var aiRev = ai.split("").reverse();
-  var biRev = bi.split("").reverse();
-  var len = Math.max(aiRev.length, biRev.length);
-  var resi = "";
-  for (var i = 0; i < len; i++) {
-    var d1 = aiRev[i] ? Number(aiRev[i]) : 0;
-    var d2 = biRev[i] ? Number(biRev[i]) : 0;
-    var sum = d1 + d2 + carry;
-    carry = sum >= 10 ? 1 : 0;
-    resi = (sum % 10) + resi;
+  if(accuCalcResult !== "") accuCalcResult = "." + accuCalcResult;
+
+  temp2 = "";
+  let temp4 = temp3.length - temp27 - 1;
+  for(let temp27 = 0; temp4 >= temp27; temp27++) {
+    if(!(temp2 === "" && temp3[temp27] === "0")) {
+      temp2 += temp3[temp27];
+    }
   }
-  if (carry) resi = "1" + resi;
+  if(temp2 === "") temp2 = "0";
+  return temp2 + accuCalcResult;
+  // var as = a.split("."),
+  //   bs = b.split(".");
+  // var ai = as[0],
+  //   bi = bs[0];
+  // var af = as[1] || "",
+  //   bf = bs[1] || "";
+  // while (af.length < bf.length) af += "0";
+  // while (bf.length < af.length) bf += "0";
 
-  // 前ゼロ削除
-  var k = 0;
-  while (k < resi.length - 1 && resi[k] === "0") k++;
-  resi = resi.slice(k);
+  // var carry = 0,
+  //   resf = "";
+  // for (var i = af.length - 1; i >= 0; i--) {
+  //   var sum = Number(af[i]) + Number(bf[i]) + carry;
+  //   carry = sum >= 10 ? 1 : 0;
+  //   resf = (sum % 10) + resf;
+  // }
 
-  // 小数部の後ゼロ削除
-  var j = resf.length - 1;
-  while (j >= 0 && resf[j] === "0") j--;
-  resf = resf.slice(0, j + 1);
+  // var aiRev = ai.split("").reverse();
+  // var biRev = bi.split("").reverse();
+  // var len = Math.max(aiRev.length, biRev.length);
+  // var resi = "";
+  // for (var i = 0; i < len; i++) {
+  //   var d1 = aiRev[i] ? Number(aiRev[i]) : 0;
+  //   var d2 = biRev[i] ? Number(biRev[i]) : 0;
+  //   var sum = d1 + d2 + carry;
+  //   carry = sum >= 10 ? 1 : 0;
+  //   resi = (sum % 10) + resi;
+  // }
+  // if (carry) resi = "1" + resi;
 
-  return resf.length > 0 ? resi + "." + resf : resi;
+  // // 前ゼロ削除
+  // var k = 0;
+  // while (k < resi.length - 1 && resi[k] === "0") k++;
+  // resi = resi.slice(k);
+
+  // // 小数部の後ゼロ削除
+  // var j = resf.length - 1;
+  // while (j >= 0 && resf[j] === "0") j--;
+  // resf = resf.slice(0, j + 1);
+
+  // return resf.length > 0 ? resi + "." + resf : resi;
 }
 
 function accuSub(a, b) {
+  if(a.includes("-")) {
+    if(b.includes("-")) return accuAdd(a.slice(1), b.slice(1));
+    else return accuSub(b, a.slice(1));
+  }
+  if(b.includes("-")) return accuAdd(a, b.slice(1));
+
   var neg = false;
   function cmp(x, y) {
     var xs = x.split("."),
@@ -115,6 +165,12 @@ function accuSub(a, b) {
 }
 
 function accuMul(a, b) {
+  if(a.includes("-")) {
+    if(b.includes("-")) return accuMul(a.slice(1), b.slice(1));
+    else return "-" + accuMul(a.slice(1), b);
+  }
+  if(b.includes("-")) return "-" + accuMul(a, b.slice(1));
+
   var as = a.split("."),
     bs = b.split(".");
   var ai = as[0] + (as[1] || "");
@@ -160,6 +216,17 @@ function accuMul(a, b) {
 }
 
 function accuDiv(a, b, c = 10) {
+  if(b == "" || b == "0") {
+    if(a == "" || a == "0") return "NaN";
+    else if (a.includes("-")) return "-Infinity";
+    else return "Infinity";
+  }
+  if(a.includes("-")) {
+    if(b.includes("-")) return accuDiv(a.slice(1), b.slice(1), c);
+    else return "-" + accuDiv(a.slice(1), b, c);
+  }
+  if(b.includes("-")) return "-" + accuDiv(a, b.slice(1), c);
+
   var as = a.split("."),
     bs = b.split(".");
   var ai = as[0] + (as[1] || "");
